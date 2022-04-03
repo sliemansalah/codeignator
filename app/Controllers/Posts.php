6b6263d31fd28808cbb1aba179f4f5c0129e6a8b
sliemansalah <slieman2001@hotmail.com>
2022-04-03 23:43:16 +0300

    up..

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -2,24 +2,15 @@
 
 namespace App\Controllers;
 
+use App\Models\PostModel;
+
 class Posts extends BaseController
 {
     public function index()
     {
         // echo view('partials/header');
 
-        $data= [
-            [
-                "id" => 1,
-                "title" => "post1",
-                "description" => "description1",
-            ],
-            [
-                "id" => 2,
-                "title" => "post2",
-                "description" => "description2",
-            ],
-        ];
-        return view('posts/index', ["posts" => $data]);
+        $data= new PostModel;
+        return view('posts/index', ["posts" => $data->findAll()]);
     }
 }
--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>Codeigniter Blog | 
     <?php $this->renderSection("title"); ?>
     </title>
@@ -12,7 +14,10 @@
 <body>
         <!-- <h1>Welcome to home page</h1> -->
 
-        <?php $this->renderSection("content"); ?>
+       <div class="container">
+            <?php $this->renderSection("content"); ?>
+       </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 </body>
